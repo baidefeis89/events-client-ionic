a7262ebe54d56ade16ab6b796b124601d3357b5e
Ivan <baidefeis89@gmail.com>
2018-02-17 20:35:21 +0100

    event-location

--- src/pages/event-location/event-location.ts
+++ src/pages/event-location/event-location.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { IEvent } from '../../models/event';
 
+import { LaunchNavigator } from '@ionic-native/launch-navigator';
+
 /**
  * Generated class for the EventLocationPage page.
  *
@@ -17,8 +19,13 @@ import { IEvent } from '../../models/event';
 export class EventLocationPage {
   event: IEvent;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private launchNavigator: LaunchNavigator) {
     this.event = navParams.data;
+  }
+
+  navigate() {
+    //let options: LaunchNavigatorOptions = {};
+    this.launchNavigator.navigate([this.event.lat, this.event.lng], {}) .then(ok => console.log("Navigation launched!"));
   }
 
 }
